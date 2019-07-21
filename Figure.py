227333a7b1125cc62c7e42c3fd6ca3cf74875f80
Douglas Luman <dluman@douglasjluman.com>
2019-07-21 14:03:59 +0000

    Trying to add a polar-gram function.

--- Figure.py
+++ Figure.py
@@ -1,7 +1,7 @@
 import Console
 from PIL import Image, ImageDraw
 
-class Draw:
+class Trigrams:
 
     def __init__(self,figures):
         self.img = None
@@ -52,3 +52,43 @@ class Draw:
             self.img.save('img/'+self.type+'/'+str(count).rjust(5,'0')+'.png')
             count += 1
             display.update(count,self.type)
+            
+class Polars:
+
+    def __init__(self,figures):
+        self.dim = 825
+        self.img = None
+        self.create(figures.trigrams)
+    
+    def setup(self):
+        self.angle = 90
+        self.radius = 37.5
+        self.xpos = self.dim/2
+        self.ypos = self.xpos-self.radius
+        self.img = Image.new('RGB',
+                             (self.dim,self.dim),
+                             (255,255,255,255))
+    
+    def create(self,set):
+        def arc(slice):
+            draw = ImageDraw.Draw(self.img)
+            draw.ellipse([(self.xpos,self.ypos),
+                          (-1*self.xpos,-1*self.ypos)],
+                          fill=0,
+                          outline=0)
+            for c in slice[0]:
+                if c == 'A':
+                    draw.arc([(self.xpos,self.ypos),
+                              (self.xpos+self.radius,self.ypos+self.radius)],
+                              self.angle,
+                              self.angle+90,
+                              fill=0)
+                self.xpos += self.radius
+                self.ypos += self.radius
+            self.angle -= 90
+            self.radius += self.radius/3
+        for g in set:
+            self.setup()
+            for elem in g:
+                arc(elem)
+        self.img.save('img/polar/test.png')
--- main.py
+++ main.py
@@ -4,12 +4,27 @@ import Figure
 import Grammar
 import System
 
+def parse(values):
+    args = {}
+    text = values[1]
+    try:
+        fmt = values[2]
+    except KeyError:
+        fmt = None
+    args = [text,fmt]
+    return args
+
 def main(args):
-    string = args[1]
+    args = parse(args)
+    string = args[0]
     grammar = Grammar.Rules(string).apply()
     system = System.Gram()
     system.evaluate(grammar[string])
-    Figure.Draw(system)
+    print args
+    if args[1] == 'polar':
+        Figure.Polars(system)
+    else:
+        Figure.Trigrams(system)
 
 if __name__== '__main__':
     main(sys.argv)
